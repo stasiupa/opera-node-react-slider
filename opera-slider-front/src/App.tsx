c9b4a3855e43934a135f92ea17f82be5e5d0e6b2
stasiupa <125651294+stasiupa@users.noreply.github.com>
2023-11-08 02:02:21 +0100

    slider-working
    slider added using "react-transition-group" lib
    slider buggy/laggy:(

--- opera-slider-front/src/App.tsx
+++ opera-slider-front/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="App">
       <ImageSlider slides={slides} />
     </div>
   );
--- opera-slider-front/src/ImageSlider.tsx
+++ opera-slider-front/src/ImageSlider.tsx
@@ -1,4 +1,8 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+import "./ImageSlider.css";
 
 interface Slide {
   title: string;
@@ -13,6 +17,7 @@ interface ImageSliderProps {
 
 function ImageSlider({ slides }: ImageSliderProps) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [direction, setDirection] = useState("slide-right");
 
   useEffect(() => {
     if (slides.length > 0) {
@@ -24,24 +29,44 @@ function ImageSlider({ slides }: ImageSliderProps) {
     setCurrentImageIndex((prevIndex) =>
       prevIndex === 0 ? slides.length - 1 : prevIndex - 1
     );
+    setDirection("slide-left");
   };
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) =>
       prevIndex === slides.length - 1 ? 0 : prevIndex + 1
     );
+    setDirection("slide-right");
   };
 
   const currentSlide = slides[currentImageIndex];
 
+  const childFactory =
+    (direction: string) =>
+    (child: React.FunctionComponentElement<{ classNames: string }>) =>
+      React.cloneElement(child, {
+        classNames: direction,
+      });
+
   return (
-    <div>
+    <div className="image-slider">
       <button onClick={previousImage}>{"<"}</button>
       {currentSlide !== undefined && (
-        <img
-          src={currentSlide.image}
-          alt={`Image ${currentImageIndex + 1}`}
-        ></img>
+        <div className="image-wrapper">
+          <TransitionGroup childFactory={childFactory(direction)}>
+            <CSSTransition
+              key={currentImageIndex}
+              timeout={1000}
+              classNames={direction}
+            >
+              <img
+                src={currentSlide.image}
+                alt={`Image ${currentImageIndex + 1}`}
+                loading="lazy"
+              />
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
       )}
       <button onClick={nextImage}>{">"}</button>
     </div>
